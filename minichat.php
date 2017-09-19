4c09839334d0fcda96fe13675b2120a7e8c8404d
seafy <vhasquette@gmail.com>
2017-09-19 12:03:08 +0200

    add a bit responsive

--- minichat.php
+++ minichat.php
@@ -19,24 +19,24 @@ $donnees=$reponse->fetchAll();
 
 // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
 ?>
-
-<div class="block">
-	<form action="minichat_post.php" method="post">
-		<input type="text" name="pseudo" placeholder="pseudo">
-		<br><textarea rows="5" cols="19" name="message" placeholder="ecrivez votre text ici !"></textarea>
-		<br><input type="submit"  value="envoyer" class="mx-5">
-	</form>
-</div>
+<div class="row mx-auto my-sm-5">
+	<div class="block ">
+		<form action="minichat_post.php" method="post">
+			<input type="text" name="pseudo" placeholder="pseudo">
+			<br><textarea rows="5" cols="19" name="message" placeholder="ecrivez votre text ici !"></textarea>
+			<br><input type="submit"  value="envoyer" class="mx-5">
+		</form>
+	</div>
 	
 	<?php 
 	foreach($donnees as $infos): ?>
-    	<div id="msg">
+    	<div id="msg" class="col-12 col-sm-12 col-md-12 col-lg-12 ">
     		<p>pseudo :<?php echo htmlspecialchars($infos['pseudo']); ?></p>
         	<p align="justify" > <?php echo htmlspecialchars($infos['message']); ?></p>
     	</div>
     	
 	<?php endforeach; ?>
-
+</div>
 
 
 
--- page/header.php
+++ page/header.php
@@ -6,4 +6,4 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
-<body>
+<body class="container">
